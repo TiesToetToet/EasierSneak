cf5bf7275e40d01342bfd99b9b5776e508712391
TiesToetToet <77145356+TiesToetToet@users.noreply.github.com>
2025-08-28 22:13:28 +0200

    Update SneakToggleHandler.java

--- fabric/src/client/java/com/tiesttoettoet/easier_sneak/SneakToggleHandler.java
+++ fabric/src/client/java/com/tiesttoettoet/easier_sneak/SneakToggleHandler.java
@@ -1,6 +1,7 @@
 package com.tiesttoettoet.easier_sneak;
 
 import net.minecraft.client.MinecraftClient;
+import org.lwjgl.glfw.GLFW;
 
 public class SneakToggleHandler {
 
@@ -14,22 +15,39 @@ public class SneakToggleHandler {
             return;
         }
 
-        boolean isKeyPressed = ModKeybinds.toggleSneakKey.isPressed();
+        int keyCode = ModKeybinds.toggleSneakKey.getDefaultKey().getCode();
+        System.out.println("Toggle Sneak Key Code: " + keyCode);
 
-        boolean isSneakPressed = mc.options.sneakKey.isPressed();
+        boolean isKeyPressed = false;
+        
+        if (keyCode >= GLFW.GLFW_KEY_A && keyCode <= GLFW.GLFW_KEY_Z) {
+            isKeyPressed = GLFW.glfwGetKey(mc.getWindow().getHandle(), keyCode) == GLFW.GLFW_PRESS;
+        } else if (keyCode == GLFW.GLFW_KEY_LEFT_CONTROL || keyCode == GLFW.GLFW_KEY_RIGHT_CONTROL) {
+            isKeyPressed = GLFW.glfwGetKey(mc.getWindow().getHandle(), GLFW.GLFW_KEY_LEFT_CONTROL) == GLFW.GLFW_PRESS ||
+                          GLFW.glfwGetKey(mc.getWindow().getHandle(), GLFW.GLFW_KEY_RIGHT_CONTROL) == GLFW.GLFW_PRESS;
+        } else if (keyCode == GLFW.GLFW_KEY_LEFT_ALT || keyCode == GLFW.GLFW_KEY_RIGHT_ALT) {
+            isKeyPressed = GLFW.glfwGetKey(mc.getWindow().getHandle(), GLFW.GLFW_KEY_LEFT_ALT) == GLFW.GLFW_PRESS ||
+                          GLFW.glfwGetKey(mc.getWindow().getHandle(), GLFW.GLFW_KEY_RIGHT_ALT) == GLFW.GLFW_PRESS;
+        } else {
+            isKeyPressed = GLFW.glfwGetKey(mc.getWindow().getHandle(), keyCode) == GLFW.GLFW_PRESS;
+        }
 
-        boolean comboPressed = isKeyPressed && isSneakPressed;
+        boolean isShiftPressed = GLFW.glfwGetKey(mc.getWindow().getHandle(),
+                GLFW.GLFW_KEY_LEFT_SHIFT) == GLFW.GLFW_PRESS ||
+                GLFW.glfwGetKey(mc.getWindow().getHandle(), GLFW.GLFW_KEY_RIGHT_SHIFT) == GLFW.GLFW_PRESS;
+
+        boolean comboPressed = isKeyPressed && isShiftPressed;
         boolean wasComboPressed = previousKeyPressed && previousShiftPressed;
 
         if (comboPressed && !wasComboPressed) {
             sneakToggleActive = !sneakToggleActive;
         }
-        else if (sneakToggleActive && isSneakPressed && !isKeyPressed && !previousShiftPressed) {
+        else if (sneakToggleActive && isShiftPressed && !isKeyPressed && !previousShiftPressed) {
             sneakToggleActive = false;
         }
 
         previousKeyPressed = isKeyPressed;
-        previousShiftPressed = isSneakPressed;
+        previousShiftPressed = isShiftPressed;
     }
 
     public static boolean getSneakState() {
